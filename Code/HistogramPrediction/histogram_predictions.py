2558db19c4956c5a4ba82a4b5d24399dd5ffb3ab
SimonCB765 <SimonCB765@gmail.com>
2016-05-12 10:29:40 +0100

    Basic histogram-based training working

--- Code/HistogramPrediction/histogram_predictions.py
+++ Code/HistogramPrediction/histogram_predictions.py
@@ -7,6 +7,16 @@ import sys
 # 3rd party imports.
 import numpy as np
 import scipy.ndimage
+from sklearn.linear_model import ElasticNet
+
+# User imports.
+import Utilities.merge_dictionaries
+import Utilities.partition_dataset
+
+# Globals.
+modelChoices = {  # The choices of models available to use.
+    "ElasticNet": {"Model": ElasticNet, "Stratified": True}
+}
 
 
 def main(arguments):
@@ -25,7 +35,19 @@ def main(arguments):
         print("Image location {0:s} is not a directory.".format(dirImages))
         sys.exit()
     fileGroundTruth = arguments["GroundTruth"]
-    backgroundThreshold = arguments["BackgroundThreshold"]
+    backgroundThreshold = arguments["BackgroundThreshold"]  # The lowest pixel value that makes up the background.
+    isPredictingHer2 = arguments["TargetHer2"]  # Whether we are attempting to predict Her2 or cell staining percentage.
+    foldsToUse = arguments["CVFolds"]  # The number of CV folds to use.
+    dirResults = arguments["ResultsLocation"]  # Directory to save the results in.
+    if not os.path.exists(dirResults):
+        # The results directory doesn't exist, so try and create it.
+        try:
+            os.makedirs(dirResults)
+        except Exception as err:
+            print("Error creating results directory: {0:s}".format(err))
+            sys.exit()
+    modelToUse = arguments["ModelToUse"]  # The type of model to train.
+    modelParams = arguments["ModelParameters"]
 
     # Extract the ground truth values.
     caseNumbers = []
@@ -34,7 +56,6 @@ def main(arguments):
     with open(fileGroundTruth, 'r') as fidGroundTruth:
         fidGroundTruth.readline()  # Strip off the header.
         for line in fidGroundTruth:
-            print(line)
             chunks = (line.strip()).split('\t')
             caseNumbers.append(int(chunks[0]))
             her2Scores.append(int(chunks[1]))
@@ -73,3 +94,42 @@ def main(arguments):
         dataMatrix[ind, 1] = caseGroundTruth[1]
         dataMatrix[ind, 2] = caseGroundTruth[2]
         dataMatrix[ind, 3:] = histogram
+
+    # Determine the target vector and the subset of the dataset used for training.
+    trainingDataMatrix = dataMatrix[:, 3:]
+    targetVector = dataMatrix[:, 1] if isPredictingHer2 else dataMatrix[:, 2]
+
+    # Determine all combinations of the model parameters. The parameters to be considered are stored as a dictionary,
+    # with the value for each dictionary entry being a list of the values to use when training the model.
+    # The goal is therefore to convert a dictionary of lists into a list of dictionaries, where each dictionary
+    # contains the same keys as the original dictionary, but has a single value associated with each key (instead of
+    # a list).
+    # For example, modelParams = {"lambda": [1, 2, 3, 4], "alpha": [0.001, 0.01, 0.1]} would become:
+    #   [{"lambda": 1, "alpha": 0.001}, {"lambda": 1, "alpha": 0.01}, {"lambda": 1, "alpha": 0.1},
+    #    {"lambda": 2, "alpha": 0.001}, {"lambda": 2, "alpha": 0.01}, {"lambda": 2, "alpha": 0.1},
+    #    {"lambda": 3, "alpha": 0.001}, {"lambda": 3, "alpha": 0.01}, {"lambda": 3, "alpha": 0.1},
+    #    {"lambda": 4, "alpha": 0.001}, {"lambda": 4, "alpha": 0.01}, {"lambda": 4, "alpha": 0.1}]
+    paramList = []
+    for i in modelParams:
+        if paramList:
+            # The parameter list already has some dictionaries in it. Therefore, create every combination of the
+            # dictionaries in the list with the dictionaries made from the values in the modelParams[i] list.
+            paramList = [Utilities.merge_dictionaries.main(k, {i: j}) for j in modelParams[i] for k in paramList]
+        else:
+            # If this is the first parameter, then take this entry of the modelParams dict and split it into a list
+            # containing one dict for each list element.
+            paramList = [{i: j} for j in modelParams[i]]
+
+    # Perform the model training.
+    if foldsToUse < 2:
+        # Train on the entire dataset and predict on the test observations.
+        for params in paramList:
+            print(params)
+            # Create the model.
+            model = modelChoices[modelToUse]["Model"](**params)
+
+            # Train the model.
+            model.fit(trainingDataMatrix, targetVector)
+    else:
+        # Train using cross validation. With two folds this is equivalent to hold out testing.
+        pass
